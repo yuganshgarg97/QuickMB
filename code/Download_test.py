71bd26822944e51d528392b8e816d812d3ef7cae
yuganshgarg97 <Yuganshgarg97@gmail.com>
2024-07-30 09:42:45 +0530

    Update Download_test.py

--- code/Download_test.py
+++ code/Download_test.py
@@ -10,7 +10,7 @@ else:
     Start = time.time()
     dummy = os.popen("wget --no-check-certificate https://releases.ubuntu.com/24.04/ubuntu-24.04-live-server-amd64.iso").read()
     End = time.time()
-    size = os.path.getsize("ubuntu-20.04.5-live-server-amd64.iso")
+    size = os.path.getsize("ubuntu-24.04-live-server-amd64.iso")
     s= size/(1024**3)
     print("The Process to Download"+str(s)+" GB Ended at ", time.ctime())
     print(" Time taken to Download "+ str(s)+ "GB Data ", End - Start," Seconds")
